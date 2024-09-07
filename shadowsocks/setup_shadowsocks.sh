91d7412aee18a908ea8a2c39d9b60f1ced9e9f0c
donchiu <donchiu@redshore.space>
2024-09-07 14:16:54 -0500

    How to use SS in terminal

---
+++ shadowsocks/setup_shadowsocks.sh
@@ -0,0 +1,6 @@
+## https://gist.github.com/hadisfr/c3f3f2a2e420d20cff45bd5ba6bb7481
+export http_proxy=socks5://127.0.0.1:1080
+export https_proxy=socks5://127.0.0.1:1080
+### sslocal -c shadowsocks.json -d start
+sslocal -c shadowsocks.json
+
