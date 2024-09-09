01eb19fa5ad41c036d08a1e5dac2d3184dccd390
donchiu <donchiu@redshore.space>
2024-09-09 20:43:36 +0000

    WIP: Extract Board

--- app/(tabs)/(explore)/tictoctoe.js
+++ app/(tabs)/(explore)/tictoctoe.js
@@ -10,17 +10,49 @@ function Square({ value, onSquareClick }) {
     )
 }
 
-const TicTacToe = () => {
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
 
     function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) return;
         const nextSquares = squares.slice();
-        nextSquares[i] = 'X';
+        nextSquares[i] = xIsNext ? 'X' : 'O';
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
 
+	const winner = calculateWinner(squares);
+	let status;
+	if (winner) {
+		status = "Winner: " + winner;
+	}
+	else {
+		status = "Next player: " + (xIsNext ? "X" : "O");
+	}
     return (
         <>
+				<div>{status}</div>
             <div>
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -38,6 +70,14 @@ const TicTacToe = () => {
             </div>
         </>
     );
+};
+
+const TicTacToe = () => {
+	return (
+		<div>
+			<Board />
+		</div>
+	)
 };
 
 const styles = StyleSheet.create({
